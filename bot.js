7be6b463e6572b3bcee13de92bceb9bc0e017623
Clem122 <69987127+Clem122@users.noreply.github.com>
2020-08-21 13:41:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,36 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const serverStats = {
+    guildID: '746030108460056718',
+    totalUsersID: '746082964839858247',
+    memberCountID: '746082991230419005',
+    time: '746087399007977595',
+    newUser: '746087439856173179'
+};
+
+bot.on('guildMemberAdd', member => {
+    if (member.guild.id !== serverStats.guildID) return;
+    client.channels.get(serverStats.totalUsersID).setName(`» Użytkowników: ${member.guild.memberCount}`);
+    client.channels.get(serverStats.memberCountID).setName(`» Ludzi: ${member.guild.members.filter(m => !m.user.bot).size}`);
+    client.channels.get(serverStats.botCount).setName(`» Botów: ${member.guild.members.filter(m => m.user.bot).size}`);
+    client.channels.get(serverStats.newUser).setName(`👋 ${member.displayName}`);
+ });
+
+bot.on('guildMemberRemove', member => {
+    if (member.guild.id !== serverStats.guildID) return;
+    client.channels.get(serverStats.totalUsersID).setName(`» Użytkowników: ${member.guild.memberCount}`);
+    client.channels.get(serverStats.memberCountID).setName(`» Ludzi: ${member.guild.members.filter(m => !m.user.bot).size}`);
+    client.channels.get(serverStats.botCount).setName(`» Botów: ${member.guild.members.filter(m => m.user.bot).size}`)
+});
 
 bot.on('message', (message) => {
     if (message.content == 'ping') {
         message.channel.sendMessage(`Pong! ${Date.now() - message.createdAt.getTime()}ms`);
     }
+});
+
+bot.on("ready", () => {
+    client.user.setStatus("dnd");
 });
 
 bot.login(process.env.BOT_TOKEN);
