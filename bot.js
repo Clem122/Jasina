22a8ab86ddf5fd8bc9543c708410637d791fec94
Clem122 <69987127+Clem122@users.noreply.github.com>
2020-08-21 20:50:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ const serverStats = {
     newUser: '746082991230419005'
 }
 
-const completemsg = `Thank you for agreeing to the rules and code of conduct! You are now a verified member of the guild! \nFeel free to choose what roles you’d like, introduce yourself or check out a our other channels. \n\n**Your unique token is your signature that you have read and understood our rules.**\n`
+const completemsg = `d**\n`
 
 const shortcode = (n) => {
     const possible = 'ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghjklmnopqrstuvwxyz0123456789'
@@ -27,13 +27,14 @@ const shortcode = (n) => {
 client.on('guildMemberAdd', (member) => {
     if (member.user.bot || member.guild.id !== config.guild) return
     const token = shortcode(8)
-    const welcomemsg = `Welcome to the guild! We hope you find a home here! Check out the \`#general\` channel to make sure that we jive, and as long as our goals are similar, then there’s a place at the table waiting for you. \n\n If you accept the code of conduct, please verify your agreement by replying to **this DM** with the verification phrase: \n\n\`I agree to abide by all rules. My token is ${token}.\`\n\n **This message is case-sensitive, and please include the period at the end! ** \n\nQuestions? Get at a staff member in the server or via DM.`
+    const channelv = member.guild.channels.find(ch => ch.name === 'weryfikacja');
+    const welcomemsg = `gowno`
     console.log(`${member.user.username}#${member.user.discriminator} joined! CODE: "${token}"`)
-    member.send(welcomemsg)
+    channelv.send(welcomemsg)
     member.user.token = token
 })
 
-const verifymsg = 'I agree to abide by all rules. My token is {token}.'
+const verifymsg = 'Akceptuje regulamin. Mój token to {token}'
 
 client.on('message', (message) => {
     if (message.author.bot || !message.author.token || message.channel.type !== `dm`) return
@@ -44,7 +45,7 @@ client.on('message', (message) => {
             description: completemsg,
             timestamp: new Date(),
             footer: {
-                text: `Verification Success`
+                text: `Weryfikacja przebiegła pomyślnie`
             }
         }
     })
@@ -192,20 +193,6 @@ if (command == "warn") {
       message.reply(`<@${wUser.id}> może znowu pisać`)
     }, ms(mutetime))
   }
-
-});
-
-client.on('message', async message => {
-
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-  let command = message.content.split(" ")[0];
-
-    if (message.channel.id === '746423667411648573')
-    if (command) {
-    message.delete(800)
-
- }
 
 });
 
