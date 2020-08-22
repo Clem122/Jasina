a49ffcd48844e9e82de68972c50de531c0b0bb61
Clem122 <69987127+Clem122@users.noreply.github.com>
2020-08-22 14:00:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -311,6 +311,39 @@ client.on('message', message => {
   }
 });
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+if (command == "propozycja") {
+    if (message.author.bot) return;
+      if(!message.channel.guild) return message.reply('ta');
+    let say = new Discord.RichEmbed()
+  .setTitle("Od " + message.author.username)
+  .setThumbnail(message.author.avatarURL)
+  .setAuthor("PROPOZYCJA")
+    .setDescription(args.join("  "))
+    .setColor(12248579)
+      .setFooter(" ")
+    message.guild.channels.find('name','propozycje').send(say).then(embedMessage => {
+        embedMessage.react("👍");
+        embedMessage.react("👎");
+    });
+    message.author.send("**- Dziękujemy za oddanie propozycji** :heart:");
+
+    message.delete();
+  }
+
+
+});
+
 client.on("ready", () => {
     client.user.setStatus("dnd");
 });
